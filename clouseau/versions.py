e4aeb9c0d7e0852e670058b7135ef8e2629269fa
calixteman <calixte.denizet@gmail.com>
2016-05-09 12:20:07 +0200

    Can get the last versions numbers for each channel

---
+++ clouseau/versions.py
@@ -0,0 +1,49 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import re
+import requests
+
+
+__versions = None
+
+
+def __getTemplateValue(url):
+    """Get version value from html file
+
+    Args:
+        url (str): the url of the html file
+
+    Returns:
+        int: the version number
+    """
+    version_regex = re.compile(".*<p>(.*)</p>.*")
+    template_page = str(requests.get(url).text.encode('utf-8')).replace('\n', '')
+    parsed_template = version_regex.match(template_page)
+    return int(parsed_template.groups()[0])
+
+
+def __getVersions():
+    """Get the versions number for each channel
+
+    Returns:
+        dict: versions for each channel
+    """
+    base_url = 'https://wiki.mozilla.org/Template:%s_VERSION'
+    names = ['RELEASE', 'BETA', 'AURORA', 'CENTRAL']
+    versions = map(lambda name: __getTemplateValue(base_url % name), names)
+    return {'release': versions[0], 'beta': versions[1], 'aurora': versions[2], 'nightly': versions[3]}
+
+
+def get():
+    """Get current version number by channel
+
+    Returns:
+        dict: containing version by channel
+    """
+    global __versions
+    if not __versions:
+        __versions = __getVersions()
+
+    return __versions
---
+++ tests/test_versions.py
@@ -0,0 +1,17 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import unittest
+import clouseau.versions as versions
+
+
+class VersionsTest(unittest.TestCase):
+
+    def test_versions(self):
+        v = versions.get()
+        self.assertTrue(v['release'] < v['beta'] < v['aurora'] < v['nightly'])
+
+
+if __name__ == '__main__':
+    unittest.main()
