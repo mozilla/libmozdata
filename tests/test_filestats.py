c0d51511c7a9460d8cfa9ea06f4f46046732a9d2
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-05-19 11:19:37 +0100

    Test FileStats with a date

--- tests/test_filestats.py
+++ tests/test_filestats.py
@@ -5,6 +5,7 @@
 import unittest
 import six
 from clouseau.FileStats import FileStats
+from clouseau import utils
 
 
 class FileStatsTest(unittest.TestCase):
@@ -12,6 +13,16 @@ class FileStatsTest(unittest.TestCase):
     def test_filestats(self):
         path = 'netwerk/protocol/http/nsHttpConnectionMgr.cpp'
         info = FileStats(path).get_info()
+        self.assertIsNot(info, None)
+        self.assertEqual(info['path'], 'netwerk/protocol/http/nsHttpConnectionMgr.cpp')
+        self.assertEqual(info['module'], 'Necko')
+        six.assertCountEqual(self, info['components'], ['Core::Networking', 'Core::Networking: Cache', 'Core::Networking: Cookies', 'Core::Networking: FTP', 'Core::Networking: File', 'Core::Networking: HTTP', 'Core::Networking: JAR', 'Core::Networking: Websockets'])
+        self.assertGreater(len(info['owners']), 0)
+        self.assertGreater(len(info['peers']), 0)
+
+    def test_filestats_date(self):
+        path = 'netwerk/protocol/http/nsHttpConnectionMgr.cpp'
+        info = FileStats(path, utc_ts=utils.get_timestamp('today')).get_info()
         self.assertIsNot(info, None)
         self.assertEqual(info['path'], 'netwerk/protocol/http/nsHttpConnectionMgr.cpp')
         self.assertEqual(info['module'], 'Necko')
