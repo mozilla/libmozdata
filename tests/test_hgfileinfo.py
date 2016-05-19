7f4a7e2ff04e08d41a466134ee70d9e227593419
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-05-19 09:59:44 +0100

    Test HGFileInfo with an array of paths

--- tests/test_hgfileinfo.py
+++ tests/test_hgfileinfo.py
@@ -10,13 +10,31 @@ class HGFileInfoTest(unittest.TestCase):
 
     def test_hgfileinfo(self):
         path = 'netwerk/protocol/http/nsHttpConnectionMgr.cpp'
-        fi = HGFileInfo(path).get()
+        hi = HGFileInfo(path)
+        fi = hi.get()
 
         self.assertTrue(path in fi)
         self.assertTrue('authors' in fi[path])
         self.assertIsNot(fi[path]['authors'], None)
         self.assertTrue('bugs' in fi[path])
         self.assertIsNot(fi[path]['bugs'], None)
+
+    def test_hgfileinfo_multiple(self):
+        path1 = 'netwerk/protocol/http/nsHttpConnectionMgr.cpp'
+        path2 = 'LICENSE'
+        hi = HGFileInfo([path1, path2])
+        fi = hi.get()
+
+        self.assertTrue(path1 in fi)
+        self.assertTrue(path2 in fi)
+        self.assertTrue('authors' in fi[path1])
+        self.assertTrue('authors' in fi[path2])
+        self.assertIsNot(fi[path1]['authors'], None)
+        self.assertIsNot(fi[path2]['authors'], None)
+        self.assertTrue('bugs' in fi[path1])
+        self.assertTrue('bugs' in fi[path2])
+        self.assertIsNot(fi[path1]['bugs'], None)
+        self.assertIsNot(fi[path2]['bugs'], None)
 
 
 if __name__ == '__main__':
