abd994d4f303ddcdc653a7c86ebd65eb299a4551
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-05-19 11:45:14 +0100

    Add tests for HGFileInfo with dates (for a very rarely changing file)

--- tests/test_hgfileinfo.py
+++ tests/test_hgfileinfo.py
@@ -4,7 +4,7 @@
 
 import unittest
 from clouseau.HGFileInfo import HGFileInfo
-
+from clouseau import utils
 
 class HGFileInfoTest(unittest.TestCase):
 
@@ -17,6 +17,63 @@ class HGFileInfoTest(unittest.TestCase):
         self.assertIsNot(fi['authors'], None)
         self.assertTrue('bugs' in fi)
         self.assertIsNot(fi['bugs'], None)
+
+    def test_hgfileinfo_date(self):
+        path = 'LICENSE'
+        hi = HGFileInfo(path)
+
+        fi = hi.get(path)
+        self.assertEqual(len(fi['authors']), 2)
+        self.assertEqual(fi['authors']['Phil Ringnalda <philringnalda@gmail.com>']['count'], 1)
+        self.assertEqual(len(fi['authors']['Phil Ringnalda <philringnalda@gmail.com>']['reviewers']), 1)
+        self.assertEqual(fi['authors']['Phil Ringnalda <philringnalda@gmail.com>']['reviewers']['gerv'], 1)
+        self.assertEqual(fi['authors']['hg@mozilla.com']['count'], 1)
+        self.assertEqual(fi['authors']['hg@mozilla.com']['reviewers'], {})
+        self.assertEqual(fi['bugs'], set(['547914']))
+        self.assertEqual(len(fi['patches']), 2)
+        self.assertEqual(fi['patches'][0]['author'], 'Phil Ringnalda <philringnalda@gmail.com>')
+        self.assertEqual(fi['patches'][1]['author'], 'hg@mozilla.com')
+
+        fi = hi.get(path, utils.get_timestamp('2009-01-01'))
+        self.assertEqual(len(fi['authors']), 1)
+        self.assertEqual(fi['authors']['Phil Ringnalda <philringnalda@gmail.com>']['count'], 1)
+        self.assertEqual(len(fi['authors']['Phil Ringnalda <philringnalda@gmail.com>']['reviewers']), 1)
+        self.assertEqual(fi['authors']['Phil Ringnalda <philringnalda@gmail.com>']['reviewers']['gerv'], 1)
+        self.assertEqual(fi['bugs'], set(['547914']))
+        self.assertEqual(len(fi['patches']), 1)
+        self.assertEqual(fi['patches'][0]['author'], 'Phil Ringnalda <philringnalda@gmail.com>')
+
+        fi = hi.get(path, utils.get_timestamp('2008-01-01'), utils.get_timestamp('2009-01-01'))
+        self.assertEqual(fi['authors']['hg@mozilla.com']['count'], 1)
+        self.assertEqual(fi['authors']['hg@mozilla.com']['reviewers'], {})
+        self.assertEqual(fi['bugs'], set())
+        self.assertEqual(len(fi['patches']), 1)
+        self.assertEqual(fi['patches'][0]['author'], 'hg@mozilla.com')
+
+        fi = hi.get(path, utc_ts_to=utils.get_timestamp('2009-01-01'))
+        self.assertEqual(len(fi['authors']), 1)
+        self.assertEqual(fi['authors']['hg@mozilla.com']['count'], 1)
+        self.assertEqual(fi['authors']['hg@mozilla.com']['reviewers'], {})
+        self.assertEqual(fi['bugs'], set())
+        self.assertEqual(len(fi['patches']), 1)
+        self.assertEqual(fi['patches'][0]['author'], 'hg@mozilla.com')
+
+        fi = hi.get(path, utils.get_timestamp('2006-01-01'), utils.get_timestamp('2007-01-01'))
+        self.assertEqual(fi['authors'], {})
+        self.assertEqual(fi['bugs'], set())
+        self.assertEqual(fi['patches'], [])
+
+        fi = hi.get(path, utils.get_timestamp('2008-01-01'), utils.get_timestamp('2012-01-01'))
+        self.assertEqual(len(fi['authors']), 2)
+        self.assertEqual(fi['authors']['Phil Ringnalda <philringnalda@gmail.com>']['count'], 1)
+        self.assertEqual(len(fi['authors']['Phil Ringnalda <philringnalda@gmail.com>']['reviewers']), 1)
+        self.assertEqual(fi['authors']['Phil Ringnalda <philringnalda@gmail.com>']['reviewers']['gerv'], 1)
+        self.assertEqual(fi['authors']['hg@mozilla.com']['count'], 1)
+        self.assertEqual(fi['authors']['hg@mozilla.com']['reviewers'], {})
+        self.assertEqual(fi['bugs'], set(['547914']))
+        self.assertEqual(len(fi['patches']), 2)
+        self.assertEqual(fi['patches'][0]['author'], 'Phil Ringnalda <philringnalda@gmail.com>')
+        self.assertEqual(fi['patches'][1]['author'], 'hg@mozilla.com')
 
     def test_hgfileinfo_multiple(self):
         path1 = 'netwerk/protocol/http/nsHttpConnectionMgr.cpp'
