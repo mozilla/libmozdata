61d9a051be68378c6890420cfdc4e37ecd520c6f
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-05-20 11:09:40 +0100

    Add detailed tests for FileStats

--- tests/test_filestats.py
+++ tests/test_filestats.py
@@ -21,14 +21,37 @@ class FileStatsTest(unittest.TestCase):
         self.assertGreater(len(info['peers']), 0)
 
     def test_filestats_date(self):
-        path = 'netwerk/protocol/http/nsHttpConnectionMgr.cpp'
+        path = 'LICENSE'
         info = FileStats(path, utc_ts=utils.get_timestamp('today')).get_info()
-        self.assertIsNot(info, None)
-        self.assertEqual(info['path'], 'netwerk/protocol/http/nsHttpConnectionMgr.cpp')
-        self.assertEqual(info['module'], 'Necko')
-        six.assertCountEqual(self, info['components'], ['Core::Networking', 'Core::Networking: Cache', 'Core::Networking: Cookies', 'Core::Networking: FTP', 'Core::Networking: File', 'Core::Networking: HTTP', 'Core::Networking: JAR', 'Core::Networking: Websockets'])
-        self.assertGreater(len(info['owners']), 0)
-        self.assertGreater(len(info['peers']), 0)
+        self.assertEqual(info['components'], set())
+        self.assertEqual(info['needinfo'], None)
+        self.assertEqual(info['path'], 'LICENSE')
+        self.assertEqual(info['guilty'], None)
+
+        info = FileStats(path, utc_ts=utils.get_timestamp('2010-04-06')).get_info()
+        self.assertEqual(info['components'], set(['Core::General']))
+        self.assertEqual(info['needinfo'], 'philringnalda@gmail.com')
+        self.assertEqual(info['path'], 'LICENSE')
+        self.assertEqual(len(info['guilty']['patches']), 1)
+        self.assertEqual(info['guilty']['main_author'], 'philringnalda@gmail.com')
+        self.assertEqual(info['guilty']['last_author'], 'philringnalda@gmail.com')
+
+        self.assertEqual(info, FileStats(path, utc_ts=utils.get_timestamp('2010-04-07')).get_info())
+        self.assertEqual(info, FileStats(path, utc_ts=utils.get_timestamp('2010-04-08')).get_info())
+
+        info = FileStats(path, utc_ts=utils.get_timestamp('2010-04-09')).get_info()
+        self.assertEqual(info['components'], set())
+        self.assertEqual(info['needinfo'], None)
+        self.assertEqual(info['path'], 'LICENSE')
+        self.assertEqual(info['guilty'], None)
+
+        info = FileStats(path, utc_ts=utils.get_timestamp('2008-03-21')).get_info()
+        self.assertEqual(info['components'], set())
+        self.assertEqual(info['needinfo'], None)
+        self.assertEqual(info['path'], 'LICENSE')
+        self.assertEqual(len(info['guilty']['patches']), 1)
+        self.assertEqual(info['guilty']['main_author'], 'hg@mozilla.com')
+        self.assertEqual(info['guilty']['last_author'], 'hg@mozilla.com')
 
 
 if __name__ == '__main__':
