751300d44d6ebd8d265587dea085c2e29c328b2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-07-18 12:40:19 +0200

    Set default duration to 5 days

--- clouseau/stability/crashes.py
+++ clouseau/stability/crashes.py
@@ -243,7 +243,7 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--output-directory', action='store', required=True, help='the output directory')
     parser.add_argument('-c', '--channels', action='store', nargs='+', default=['release', 'beta', 'aurora', 'nightly'], help='the channels')
     parser.add_argument('-d', '--date', action='store', default='yesterday', help='the end date')
-    parser.add_argument('-D', '--duration', action='store', default=11, help='the duration')
+    parser.add_argument('-D', '--duration', action='store', default=5, help='the duration')
     parser.add_argument('-t', '--tclimit', action='store', default=50, help='number of top crashes to retrieve')
 
     args = parser.parse_args()
