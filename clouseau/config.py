9a5d07e8bfa2f14f940c3663c05f3e610f93a644
Calixte Denizet <calixte.denizet@gmail.com>
2016-08-29 14:10:14 +0200

    Allow to have different ways to get configuration elements

--- clouseau/config.py
+++ clouseau/config.py
@@ -8,20 +8,38 @@ try:
 except ImportError:
     from ConfigParser import ConfigParser
 
-__config = None
+
+class Config(object):
+
+    def get(section, option, default=None):
+        raise NotImplementedError
 
 
-def __read_config():
+class ConfigIni(Config):
+
+    def __init__(self, path=''):
+        if not path:
+            path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config.ini')
+        self.config = ConfigParser()
+        self.config.read(path)
+
+    def get(self, section, option, default=None):
+        if not self.config.has_option(section, option):
+            return default
+
+        return self.config.get(section, option)
+
+
+__config = ConfigIni()
+
+
+def set_config(conf):
+    if not isinstance(conf, Config):
+        raise TypeError('Argument must have type config.Config')
     global __config
-    __config = ConfigParser()
-    __config.read(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config.ini'))
+    __config = conf
 
 
 def get(section, option, default=None):
-    if __config is None:
-        __read_config()
-
-    if not __config.has_option(section, option):
-        return default
-
-    return __config.get(section, option)
+    global __config
+    return __config.get(section, option, default=default)
