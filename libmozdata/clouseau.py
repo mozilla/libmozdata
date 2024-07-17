27a9a8d5b1b5343244695174809d8f366c47d69f
Suhaib Mujahid <smujahid@mozilla.com>
2024-07-17 10:12:04 -0400

    Copy the implementation of `itertools.batched()`

--- libmozdata/clouseau.py
+++ libmozdata/clouseau.py
@@ -2,7 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from itertools import batched
+
+from libmozdata.utils import batched
 
 from . import config
 from .connection import Connection, Query
